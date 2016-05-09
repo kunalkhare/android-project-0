602f6d6828f7f9ba4b0cf0ceedfaf61c53af197c
Revival <code4lisa@gmail.com>
2016-05-08 21:36:00 -0400

    project-0

---
+++ MyAppPortfolio/app/src/main/java/com/example/android/myappportfolio/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.android.myappportfolio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickGoUbt(View view) {
+        Toast.makeText( getApplicationContext(), "This button will launch Go Ubiquitous", Toast.LENGTH_SHORT).show();
+    }
+
+    public void clickCapstone(View view) {
+        Toast.makeText( getApplicationContext(), "This button will launch Capstone", Toast.LENGTH_SHORT).show();
+    }
+
+    public void clickMakeApp(View view) {
+        Toast.makeText( getApplicationContext(), "This button will launch Make Your App", Toast.LENGTH_SHORT).show();
+    }
+
+    public void clickBuildIt(View view) {
+        Toast.makeText( getApplicationContext(), "This button will launch Build It Bigger", Toast.LENGTH_SHORT).show();
+    }
+
+    public void clickStockHawk(View view) {
+        Toast.makeText( getApplicationContext(), "This button will launch my Stock Hawk ", Toast.LENGTH_SHORT).show();
+    }
+
+    public void clickPmovies(View view) {
+        Toast.makeText( getApplicationContext(), "This button will launch my movies", Toast.LENGTH_SHORT).show();
+    }
+}
